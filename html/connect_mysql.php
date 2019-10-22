29871b31f42f101fd16031f468b170ecae79f921
root <root@vm10-0-0-21.ksc.com>
2019-10-22 15:10:49 +0800

    add php-ext mongodb redis memcached

--- html/connect_mysql.php
+++ html/connect_mysql.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("172.18.0.2:3306", "root", "123456", "mysql");
+$link = mysqli_connect("mysql:3306", "root", "123456", "mysql");
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
